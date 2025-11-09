f5605f26aaae3709d27c93c1277a96affe6a7d77
hsy822 <hyunsy822@gmail.com>
2025-11-10 07:23:24 +0900

    update confib

--- next.config.ts
+++ next.config.ts
@@ -15,14 +15,19 @@ const nextConfig: NextConfig = {
         headers: [
           { key: 'Cross-Origin-Opener-Policy', value: 'same-origin-allow-popups' },
           { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
-          { key: 'Cross-Origin-Resource-Policy', value: 'cross-origin' }, 
+          { key: 'Cross-Origin-Resource-Policy', value: 'cross-origin' },
+          { 
+            key: 'Content-Security-Policy', 
+            value: "script-src 'self' 'unsafe-eval';" 
+          }
         ],
       },
       {
-        source: '/:path((?!portal).*)',
+        source: '/:path((?!portal).*)', 
         headers: [
           { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
           { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: 'Cross-Origin-Resource-Policy', value: 'same-origin' }
         ],
       },
     ];
